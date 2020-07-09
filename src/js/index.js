d5883093f5692f3132d0dedf4fb3653350d3e16c
Grzegorz Zielinski <gregoryblack11@gmail.com>
2020-07-09 18:38:54 +0200

    features: added new pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../scss/main.scss';
+import moment from 'moment';
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -7,3 +8,21 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+console.log('Wtaj w digitalowym Świecie');
+console.log(`Remember: Every Journey starts with a single line of code`);
+console.log('zrobiłem zadania więc piję szampana')
+
+const hamburger = document.querySelector('.hamburger--js')
+
+console.log(hamburger);
+
+hamburger.addEventListener('click', () => {
+    const navigation = document.querySelector('.navigation--js');
+    navigation.classList.toggle('navigation--open');
+})
+
+const timeHeader = document.querySelector('.time');
+timeHeader.innerHTML = "The day started: " + moment().startOf('day').fromNow();
+
